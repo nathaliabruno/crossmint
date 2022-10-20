ce70460ec5d4a569503e7843d6b423d27ed7e136
Nathalia Bruno <nathi.bruno@gmail.com>
2022-10-20 17:57:13 -0300

    finish

---
+++ utils/index.ts
@@ -0,0 +1,9 @@
+export const debounce = (callback, wait) => {
+  let timerId;
+  return (...args) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, wait);
+  };
+};
